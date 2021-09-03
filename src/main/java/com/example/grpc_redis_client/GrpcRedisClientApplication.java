d4d70de88cd88b0df43a515191575d86ced8bcf0
Mihran Maruqyan <mihran_maruqyan@epam.com>
2021-09-03 11:08:02 +0400

    Initial commit

---
+++ src/main/java/com/example/grpc_redis_client/GrpcRedisClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc_redis_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcRedisClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcRedisClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grpc_redis_client/GrpcRedisClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpc_redis_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcRedisClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
